32496c6b31faa6e1929f04a90782d3a02f5e3f19
venelinpetrov <vpetrov33tu@gmail.com>
2025-06-26 13:23:53 +0300

    Add flip animatin

--- src/Card.ts
+++ src/Card.ts
@@ -2,12 +2,31 @@ export class Card {
     private opened = false;
     private removed = false;
     private el: HTMLDivElement;
+    private innerEl: HTMLDivElement;
+    private frontEl: HTMLDivElement;
+    private backEl: HTMLDivElement;
     private onClick: () => void = () => { };
 
     constructor(public readonly name: string) {
         this.el = document.createElement('div');
         this.el.classList.add('card');
         this.el.addEventListener('click', () => this.handleClick());
+
+        this.innerEl = document.createElement('div');
+        this.innerEl.classList.add('card-inner');
+
+        this.frontEl = document.createElement('div');
+        this.frontEl.classList.add('card-front');
+        this.frontEl.style.backgroundImage = "url('../public/cards/back/blue.svg')";
+
+        this.backEl = document.createElement('div');
+        this.backEl.classList.add('card-back');
+        this.backEl.style.backgroundImage = `url(../public/cards/front/${this.name}.svg)`;
+
+        this.innerEl.appendChild(this.frontEl);
+        this.innerEl.appendChild(this.backEl);
+        this.el.appendChild(this.innerEl);
+
         this.update();
     }
 
@@ -28,9 +47,7 @@ export class Card {
     }
 
     update() {
-        this.el.style.backgroundImage = this.opened ?
-            `url(../public/cards/front/${this.name}.svg)`
-            : `url('../public/cards/back/blue.svg')`;
+        this.el.classList.toggle('opened', this.opened);
         this.el.classList.toggle('removed', this.removed);
     }
 
